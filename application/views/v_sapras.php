aa3accc79c5350e8e9cb4411c2ec41584e09810e
udin9951 <kamaludin9951@gmail.com>
2022-08-17 00:48:32 +0700

    Update v_sapras.php

--- application/views/v_sapras.php
+++ application/views/v_sapras.php
@@ -15,10 +15,10 @@
                 <?php $no=1; foreach ($sapras as $key => $value) { ?>
                 <div class="col-6 col-md-4">
                     <div class="services__item card p2">
-                        <img class="w-100" src="<?= file_exists(FCPATH.'/gambar/'. $value->foto_sapras) ? base_url('gambar/'. $value->foto_sapras) : "https://via.placeholder.com/300x300.png/001177?text=$value->nama_sapras"; ?>">
+                        <img style="object-fit: revert !important;" class="img-thumbnail w-100" src="<?= file_exists(FCPATH.'/sampul/'. $value->foto_sapras) ? base_url('sampul/'. $value->foto_sapras) : "https://via.placeholder.com/300x300.png/001177?text=$value->nama_sapras"; ?>">
                         <div class="card-body blog__item__text">
-                        <h5 class="card-title"><?= $value->nama_sapras?> </h5>
-                        <p class="card-text"><?= ucfirst($value->deskripsi_sapras)?></p>
+                        <h5 class="card-title" style="margin-bottom : 0px !important"><?= $value->nama_sapras?> </h5>
+                        <p class="card-text" style="margin-top : 0px !important"><?= ucfirst($value->deskripsi_sapras)?></p>
                         </div>
                     </div>
                 </div>
