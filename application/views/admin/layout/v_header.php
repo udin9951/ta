a1e7f3b2bafcea6fbac8023ca91841cda4658ad4
udin9951 <kamaludin9951@gmail.com>
2022-08-20 19:43:43 +0700

    Update v_header.php

--- application/views/admin/layout/v_header.php
+++ application/views/admin/layout/v_header.php
@@ -12,7 +12,10 @@
             <!-- menu profile quick info -->
             <div class="profile clearfix">
               <div class="profile_pic">
-                <img src="<?= base_url().'/gambar/'.$this->session->userdata('foto')?>" alt="..." class="img-circle profile_img">
+                <?php
+                  $username = $this->session->userdata('username');
+                  $url = file_exists(FCPATH.'/gambar/'. $this->session->userdata('foto')) ? base_url('gambar/'. $this->session->userdata('foto')) : "https://via.placeholder.com/300x300.png/001177?text=$username"; ?>
+                <img src="<?= $url;?>" alt="..." class="img-circle profile_img">
               </div>
                 <div class="profile_info mt-3">
                   <h2> <?= $this->session->userdata('nama_user');?></h2>
